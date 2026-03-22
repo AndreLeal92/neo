9fdd9019f1357922b48ccd25559226b2bbf2ab3e
Andre Leal <andre.leal1992@gmail.com>
2026-03-22 15:08:58 -0300

    Primeiro commit: Estrutura inicial PHP

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
-session_start();
-
-// Se o usuário não estiver logado, redireciona para o login
-if (!isset($_SESSION['username'])) {
-    header('Location: login.php');
-    exit();
-}
-
-echo "<h1>Bem-vindo, " . $_SESSION['username'] . "!</h1>";
-echo "<a href='logout.php'>Sair</a>";
+echo "<h1>Site Neofleet Online</h1>";
 ?>
